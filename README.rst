f8b70dd55223acb25d7f96085835d808a7134517
Patricio Paez <pp@mc.lan>
2012-11-11 21:17:14 -0600

    Initial commit
    - Starting with wsine.py, that I experimented
      with on October 2010.

---
+++ README.rst
@@ -0,0 +1,9 @@
+Using Python in music
+=====================
+
+Examples of audio and MIDI with Python.
+
+The examples are being published in
+the wiki at LinuxCabal:
+
+https://wiki.cabal.mx/wiki/Python_en_la_música
---
+++ wsine.py
@@ -0,0 +1,54 @@
+import sys
+import math
+from fractions import Fraction as F
+
+f = open('sine.raw', 'wb')
+f2 = open('sine', 'w')
+
+def create_sine( frequency, amplitude=30000, duration=1000, sampleRate=44100 ):
+
+    samplesPerCycle = F(sampleRate)/F(frequency)
+    samples = samplesPerCycle.numerator
+    cycles = samplesPerCycle.denominator
+
+    print frequency, 'cycles/s', sampleRate, 'samples/s:', \
+        samples, 'samples for', cycles, 'cycles'
+
+    ivalues = []
+    values = []
+    for sample in range( samples ):
+        t = 2 * math.pi * sample / samplesPerCycle
+        value = int( math.sin( t ) * amplitude )
+        low_byte = value & 255
+        high_byte = (value >> 8 ) & 255
+        # print sample, t, value, high_byte, low_byte
+        values.append( ( low_byte, high_byte ) )
+        ivalues.append( value )
+
+    repeat = int( sampleRate / samples * duration / 1000. )
+    print '  repeat', repeat, 'times for one second.'
+    for i in range( repeat ):
+        for low_byte, high_byte in values:
+            f.write( chr(low_byte) )
+            f.write( chr(high_byte) )
+        for value in ivalues:
+            f2.write( str(value) + '\n' )
+
+
+create_sine( 10, amplitude=30000 )
+create_sine( 100, amplitude=30000 )
+create_sine( 200, amplitude=30000 )
+create_sine( 300, amplitude=30000 )
+create_sine( 333, amplitude=30000 )
+create_sine( 1000, amplitude=30000 )
+create_sine( 2000, amplitude=30000 )
+create_sine( 3000, amplitude=30000 )
+create_sine( 4000, amplitude=30000 )
+create_sine( 5000, amplitude=30000 )
+create_sine( 440, amplitude=30000, duration=500 )
+create_sine( 880, amplitude=30000, duration=500 )
+create_sine( 1320, amplitude=30000, duration=500 )
+create_sine( 880, amplitude=30000, duration=500 )
+create_sine( 440, amplitude=30000, duration=500 )
+create_sine( 880, amplitude=30000, duration=500 )
+create_sine( 440, amplitude=30000, duration=1000 )
